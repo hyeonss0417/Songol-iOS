c7494856e0222d000b6472d07f1ee78490614f5b
최민섭 <minseop@choeminseob-ui-macbook-pro.local>
2018-08-30 23:51:58 +0900

    Initial Commit

---
+++ Songol-ETests/Songol_ETests.swift
@@ -0,0 +1,36 @@
+//
+//  Songol_ETests.swift
+//  Songol-ETests
+//
+//  Created by 최민섭 on 2018. 8. 30..
+//  Copyright © 2018년 최민섭. All rights reserved.
+//
+
+import XCTest
+@testable import Songol_E
+
+class Songol_ETests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
